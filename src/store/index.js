6c535f86bf9cb551aa8db3fd5a37a90692cbf6bf
developerkamlesh <kamlesh@recruitcrm.io>
2023-03-31 13:19:46 +0530

    all folder and structure define by kamlesh

--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,48 @@
-import router from '@/router';
 import { createStore } from 'vuex';
+
+import directorModule from './modules/director/index.js';
+import managerModule from './modules/manager/index.js';
+import workerModule from './modules/worker/index.js';
+import adminModule from './modules/admin/index.js';
+import authModule from './modules/auth/index.js';
 
 
 const store = createStore({
-    
-  state() {
-    return {
-      isLoggedIn: false
-    };
-  },
-  mutations: {
-    authenticate(state, loginStatus) {
-      state.isLoggedIn = loginStatus;
-    }
-  },
-  actions: {
-    login({ commit }) {
-      commit('authenticate', true);
-    },
-    logout({ commit }) {
-      commit('authenticate', false);
-      router.push({name:'login'})
-    }
-  },
-  getters: {
-    isAuthenticated(state) {
-      return state.isLoggedIn;
-    }
+  modules: {
+    director: directorModule,
+    manager: managerModule,
+    worker: workerModule,
+    admin : adminModule,
+    auth : authModule,
   }
 });
+
+// const store = createStore({
+    
+//   state() {
+//     return {
+//       isLoggedIn: false
+//     };
+//   },
+//   mutations: {
+//     authenticate(state, loginStatus) {
+//       state.isLoggedIn = loginStatus;
+//     }
+//   },
+//   actions: {
+//     login({ commit }) {
+//       commit('authenticate', true);
+//     },
+//     logout({ commit }) {
+//       commit('authenticate', false);
+//       router.push({name:'login'})
+//     }
+//   },
+//   getters: {
+//     isAuthenticated(state) {
+//       return state.isLoggedIn;
+//     }
+//   }
+// });
 
 export default store;
