3f5eed848f322f0d7f05a5a88a885aa9a2f2b1c2
Mayank Walia CRM <mayank@recruitcrm.io>
2023-04-13 09:24:59 +0530

    a

--- src/pages/director/ViewProjects.vue
+++ src/pages/director/ViewProjects.vue
@@ -207,7 +207,7 @@ export default {
     backgroundChange(tasks_completed, total_tasks){
       if((tasks_completed / total_tasks) * 100 < 30){
         return "progress-bar bg-danger"
-      }else if((tasks_completed / total_tasks) * 100<60){
+      }else if((tasks_completed / total_tasks) * 100 <script 60){
         return "progress-bar bg-warning"
       }else{
         return "progress-bar bg-success"
@@ -345,50 +345,49 @@ export default {
     async reloadComponent() {
       await this.loadProject();
     }
-  },
+  };
   // async mounted() {
   //   await this.loadProject()
-  //   const ctx = document.getElementById('myChart');
-  //   const myChart = new Chart(ctx, {
-  //     type: 'bar',
-  //     data: {
-  //       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  //       datasets: [{
-  //         label: '# of Votes',
-  //         data: [12, 19, 3, 5, 2, 3],
-  //         backgroundColor: [
-  //           'rgba(255, 99, 132, 0.2)',
-  //           'rgba(54, 162, 235, 0.2)',
-  //           'rgba(255, 206, 86, 0.2)',
-  //           'rgba(75, 192, 192, 0.2)',
-  //           'rgba(153, 102, 255, 0.2)',
-  //           'rgba(255, 159, 64, 0.2)'
-  //         ],
-  //         borderColor: [
-  //           'rgba(255, 99, 132, 1)',
-  //           'rgba(54, 162, 235, 1)',
-  //           'rgba(255, 206, 86, 1)',
-  //           'rgba(75, 192, 192, 1)',
-  //           'rgba(153, 102, 255, 1)',
-  //           'rgba(255, 159, 64, 1)'
-  //         ],
-  //         borderWidth: 1
-  //       }]
-  //     },
-  //     options: {
-  //       scales: {
-  //         x: {
-  //           type: 'category'
-  //         },
-  //         y: {
-  //           ticks: {
-  //             beginAtZero: true
-  //           }
-  //         }
-  //       }
-  //     }
-  //   });
-  //   console.log(myChart);
-  // }
+    // const ctx = document.getElementById('myChart');
+    // const myChart = new Chart(ctx, {
+    //   type: 'bar',
+    //   data: {
+    //     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    //     datasets: [{
+    //       label: '# of Votes',
+    //       data: [12, 19, 3, 5, 2, 3],
+    //       backgroundColor: [
+    //         'rgba(255, 99, 132, 0.2)',
+    //         'rgba(54, 162, 235, 0.2)',
+    //         'rgba(255, 206, 86, 0.2)',
+    //         'rgba(75, 192, 192, 0.2)',
+    //         'rgba(153, 102, 255, 0.2)',
+    //         'rgba(255, 159, 64, 0.2)'
+    //       ],
+    //       borderColor: [
+    //         'rgba(255, 99, 132, 1)',
+    //         'rgba(54, 162, 235, 1)',
+    //         'rgba(255, 206, 86, 1)',
+    //         'rgba(75, 192, 192, 1)',
+    //         'rgba(153, 102, 255, 1)',
+    //         'rgba(255, 159, 64, 1)'
+    //       ],
+    //       borderWidth: 1
+    //     }]
+    //   },
+    //   options: {
+    //     scales: {
+    //       x: {
+    //         type: 'category'
+    //       },
+    //       y: {
+    //         ticks: {
+    //           beginAtZero: true
+    //         }
+    //       }
+    //     }
+    //   }
+    // });
+    // console.log(myChart);
 }
 </script>
